b217c49289c6717a1f4816cd47fcbf9a9487683b
brinem <brinemsafari@gmail.com>
2024-02-23 10:46:27 +0300

    first commit

---
+++ neogym/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from neogym import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('trainer/', views.trainer, name='trainer'),
+    path('why/', views.why, name='why')
+]
